999baeeac5901b72f0244eacc23409bb8367ed6f
Yusuf <holla2specialyusuf@gmailcom>
2024-01-07 07:09:09 +0100

    add comments

--- server.js
+++ server.js
@@ -1,29 +1,18 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-// const MongoClient = require('mongodb').MongoClient
 const AnimeWord = require('./models/words.js');
 const mongoose = require("mongoose");
-
-
 require('dotenv').config()
 
 
+// middleware
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({ extended: true}))
-// app.use(bodyParser.json()) 
 
 
-// MongoClient.connect(connectionString, {useUnifiedTopology: true })
-//     .then(client => {
-//         console.log('Connected to Anime Database')
-//         const db = client.db('anime--words')
-//         const wordsCollection = db.collection('words')
-       
-//     })        .catch(console.error( error => console.error(error)))
-        
+// connect to the database
 mongoose 
 .connect(process.env.DBCONNECTION)
 .then(() => {
@@ -34,41 +23,32 @@ mongoose
 });
 
 
+// homepage
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
+// show all words
 app.get('/words', (req, res) => {
 
 AnimeWord
   .find() 
   .then(results => {
     console.log(results)
-    // res.render('words',  {words:results})
+     res.render('words',  {words:results})
     }).catch(error => {
       res.send(error)
       console.error(error)})   
-    // db.collection('words')
-    // .find()
-    // .toArray()
-    // .then(results => {
-    //     // console.log(results);
-    //     res.render('words.ejs', {words: results})
-    // }).catch(error => console.error(error)) 
-            res.render('words')
-
 })
+
+// add words 
 app.get('/add', (req, res) => {
     res.render('add')
 })
-app.get('/dashboard', (req, res) => {
-res.render('dashboard')
-})
 
-app.get('/delete', (req, res) => {
-    res.render('delete')
-})
-        
+
+  
+// add a word
 app.post('/word', (req, res) => {
     // console.log(req.body);
     // console.log(req.body.animeWord); 
@@ -79,25 +59,29 @@ app.post('/word', (req, res) => {
         });
         word.save()
         .then(() => {
-            res.send('Item succedfully added to the database');
+            console.log('Item succedfully added to the database');
+            res.redirect('/word')
             })
         .catch((err) => {
             console.log(err);
         });
     
     })
-//     AnimeWord
-//     .insertOne(req.body) 
-//     .then(result => {
-//         console.log(result)
-//         res.redirect('/')
-//     }) 
-//     .catch(error => console.error(error))
-// })   
 
-        app.listen(4000, () => {
-            console.log('Connected to port 4000');
-        })
+
+    // show the dashboard
+// app.get('/dashboard', (req, res) => {
+// res.render('dashboard')
+// })
+
+// delete a wpord
+// app.get('/delete', (req, res) => {
+//     res.render('delete')
+// })
+
+app.listen(4000, () => {
+    console.log('Connected to port 4000');
+})
 
 
 
